8b2c62c63c53c6c9c89bfc36959be1cf01200b7c
scara02 <221284@astanait.edu.kz>
2023-04-15 23:34:45 +0600

    Adding new methods to MyList interface

--- src/MyList.java
+++ src/MyList.java
@@ -1,6 +1,13 @@
 public interface MyList<E> extends Iterable<E>{
     void add(E element);
+    void add(E element, int index);
     E get(int index);
-    void remove(int index);
+    boolean remove(E element);
+    E remove(int index);
     int size();
+    boolean contains(Object o);
+    void clear();
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
 }
